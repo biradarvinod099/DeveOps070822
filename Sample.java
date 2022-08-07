8ba511c7202d48c5c71ec3f4fff77c7dc91cb90d
vinodkumar2022 <vinodu5009@gmail.com>
2022-08-07 19:57:24 +0530

    Modified and Added one line new extra to Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,8 @@ class Sample
 {
  public static void main(String args[])
   {
-    System.out.println("HelloWorld");
+    
+	   System.out.println("HelloWorld");
+           System.out.println("WelComeTOGit");
   }
 } 
